0b7a4fdd2ecaa37923b0d89fc2ea56145009e2a2
xSky <0xsky@live.com>
2021-06-11 22:04:09 -0400

    日志输出添加颜色

--- src/xLog.cpp
+++ src/xLog.cpp
@@ -1,24 +1,25 @@
 
 
 #include "xLog.h"
-#include <time.h>
 #include <stdarg.h>
+#include <time.h>
 
-const char* const _errstr[] = { "ERR", "WARN", "INFO", "DEBUG" };
+
+const char* const _errstr[] = { "\033[01;31mERROR\033[0m", "\033[01;33mWARN \033[0m", "\033[01;37mINFO \033[0m", "\033[22;34mDEBUG\033[0m" };
 static void (*log_fn)(int level, const char* line) = NULL;
-uint32_t _level = LOG_LEVEL_DEBUG;
+uint32_t _level = LOG_LEVEL_INFO;
 
 void set_log_level(uint32_t level, void (*call_fn)(int level, const char* line))
 {
     _level = level;
     log_fn = call_fn;
-    xredis_info("set log level:%s", level);
 }
 
-void log_message(uint32_t level, const char* fmt, ...)
+void log_message(uint32_t level, const char* function, int line, const char* fmt, ...)
 {
-    if (level > _level)
+    if (level > _level) {
         return;
+    }
 
     time_t t;
     time(&t);
@@ -43,17 +44,24 @@ void log_message(uint32_t level, const char* fmt, ...)
         if (i > 0) {
             if (data1[i - 1] != '\n') {
                 data1[i++] = '\n';
-                data1[i++] = '\0';
+                data1[i] = '\0';
             }
         }
     }
 
+    while (data1[i - 2] == '\n')
+        i--;
+    data1[i] = '\0';
+
     if (log_fn) {
         log_fn(level, data1);
     } else {
-        printf("[%04d-%02d-%02d %02d:%02d:%02d] %-5s %s\r\n",
+        printf("[%04d-%02d-%02d %02d:%02d:%02d] %-5s %s:%d %s",
             tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
             tm.tm_hour, tm.tm_min, tm.tm_sec,
-            _errstr[level], data1);
+            _errstr[level], function, line, data1);
     }
 }
+
+uint64_t atoull(const char* str) { return (NULL == str) ? (0) : (strtoull(str, (char**)NULL, 0)); }
+uint32_t atoul(const char* str) { return (NULL == str) ? (0) : ((uint32_t)atol(str)); }
--- src/xLog.h
+++ src/xLog.h
@@ -2,22 +2,21 @@
 #ifndef _XREDIS_LOG_H_
 #define _XREDIS_LOG_H_
 
+#include "xRedisClient.h"
 #include <stdint.h>
 #include <stdio.h>
 
-#define LOG_LEVEL_ERROR 0
-#define LOG_LEVEL_WARN 1
-#define LOG_LEVEL_INFO 2
-#define LOG_LEVEL_DEBUG 3
-
-#define xredis_error(...) log_message(LOG_LEVEL_ERROR, __VA_ARGS__)
-#define xredis_warn(...)  log_message(LOG_LEVEL_WARN, __VA_ARGS__)
-#define xredis_info(...)  log_message(LOG_LEVEL_INFO, __VA_ARGS__)
-#define xredis_debug(...) log_message(LOG_LEVEL_DEBUG, __VA_ARGS__)
+#define xredis_error(...) log_message(LOG_LEVEL_ERROR, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define xredis_warn(...)  log_message(LOG_LEVEL_WARN, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define xredis_info(...)  log_message(LOG_LEVEL_INFO, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define xredis_debug(...) log_message(LOG_LEVEL_DEBUG, __FUNCTION__, __LINE__, __VA_ARGS__)
 
 #define DATA_BUF 4096
 
-void log_message(uint32_t level, const char* fmt, ...);
+void log_message(uint32_t level, const char* function, int line, const char* fmt, ...);
 void set_log_level(uint32_t level, void (*emit)(int level, const char* line));
+
+uint64_t atoull(const char* str);
+uint32_t atoul(const char * str);
 
 #endif
